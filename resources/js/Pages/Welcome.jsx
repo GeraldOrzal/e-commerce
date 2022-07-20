418915a1d98148ceb0a57401f0cfff0b766dc0c3
JanAntonioTablizo <janantoniotablizo@gmail.com>
2022-07-20 21:43:10 +0800

    y

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -22,35 +22,30 @@ export default function Welcome(props) {
             </header>
             <main>
                 <div className='flex lg:flex-row justify-center items-center mb-7 space-x-2'>
-                    <div className='border border-fourthdary rounded-md p-32 w-3/6 text-center'>Ads</div>
-                    <div className='border border-fourthdary rounded-md p-32 w-3/6 text-center'>Events</div>
+                    <div className='bg-fourthdary rounded-md p-32 w-3/6 text-center'>Ads</div>
+                    <div className='bg-fourthdary rounded-md p-32 w-3/6 text-center'>Events</div>
                 </div>
                 <div className='p-2 mb-2'>Featured Products</div>
                 <div className='flex flex-row items-center space-x-2 overflow-x-scroll mb-7'>
-                    <div className='p-32 border rounded-md'></div>
-                    <div className='p-32 border rounded-md'></div>
-                    <div className='p-32 border rounded-md'></div>
-                    <div className='p-32 border rounded-md'></div>
-                    <div className='p-32 border rounded-md'></div>
-                    <div className='p-32 border rounded-md'></div>
-                    <div className='p-32 border rounded-md'></div>
-                    <div className='p-32 border rounded-md'></div>
-                    <div className='p-32 border rounded-md'></div>
-                    <div className='p-32 border rounded-md'></div>
-                    <div className='p-32 border rounded-md'></div>
-                    <div className='p-32 border rounded-md'></div>
-                    <div className='p-32 border rounded-md'></div>
-                    <div className='p-32 border rounded-md'></div>
-                    <div className='p-32 border rounded-md'></div>
+                    <div className='p-32 bg-fourthdary rounded-md'>1</div>
+                    <div className='p-32 bg-fourthdary rounded-md'>2</div>
+                    <div className='p-32 bg-fourthdary rounded-md'>3</div>
+                    <div className='p-32 bg-fourthdary rounded-md'>4</div>
+                    <div className='p-32 bg-fourthdary rounded-md'>5</div>
+                    <div className='p-32 bg-fourthdary rounded-md'>6</div>
+                    <div className='p-32 bg-fourthdary rounded-md'>7</div>
+                    <div className='p-32 bg-fourthdary rounded-md'>8</div>
+                    <div className='p-32 bg-fourthdary rounded-md'>9</div>
+                    <div className='p-32 bg-fourthdary rounded-md'>10</div>
                 </div>
-                <div className='p-2'>Shop by Categories</div>
+                <div className='p-2 mb-2'>Shop by Categories</div>
                 <div className='grid grid-cols-6 grid-rows-2 auto-rows-max gap-3'>
-                    <div className='text-center border p-20 col-start-1 col-end-3 row-start-1 row-end-3'>1</div>
-                    <div className='text-center border p-20 col-start-3 col-end-5'>2</div>
-                    <div className='text-center border p-20 col-start-5 col-end-7'>3</div>
-                    <div className='text-center border p-20 col-start-3 col-end-5'>4</div>
-                    <div className='text-center border p-20'>5</div>
-                    <div className='text-center border p-20'>6</div>
+                    <div className='text-center bg-fourthdary rounded-md p-20 col-start-1 col-end-3 row-start-1 row-end-3'>1</div>
+                    <div className='text-center bg-fourthdary rounded-md p-20 col-start-3 col-end-5'>2</div>
+                    <div className='text-center bg-fourthdary rounded-md p-20 col-start-5 col-end-7'>3</div>
+                    <div className='text-center bg-fourthdary rounded-md p-20 col-start-3 col-end-5'>4</div>
+                    <div className='text-center bg-fourthdary rounded-md p-20'>5</div>
+                    <div className='text-center bg-fourthdary rounded-md p-20'>6</div>
                 </div>
             </main>
         </div>
