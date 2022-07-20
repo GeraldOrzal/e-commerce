5044a15489ad6ea2ab5543a96edd078754794892
JanAntonioTablizo <janantoniotablizo@gmail.com>
2022-07-20 18:01:20 +0800

    y

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -9,7 +9,7 @@ export default function Welcome(props) {
         <div className='w-full p-2 border m-auto '>
             <header className='container p-5 m-auto mb-2 flex flex-row justify-center items-center'>
                 <img src='#' alt='LOGO' className='border mr-2'/>
-                <input type='text' className='bg-thirdary pl-5 outline-0 rounded-bl-xl p-2 w-3/5' placeholder='Search shop/product name'/>
+                <input type='text' className='bg-thirdary pl-5 outline-0 rounded-bl-xl p-2 w-3/5' placeholder='Search shop / product name'/>
                 <button type='submit' className='bg-thirdary rounded-tr-xl pr-5 px-2 py-2.5 text-xl'><GoSearch/></button>
                 <nav>
                 <Link href={route('login')} className=" ml-4 underline text-fourthdary">
