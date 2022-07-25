9a7c501096c818fe28417d077522d308313bb329
JanAntonioTablizo <janantoniotablizo@gmail.com>
2022-07-25 16:46:48 +0800

    dgfks

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -24,8 +24,8 @@ export default function Welcome(props) {
                 </nav>
             </header>
             <main>
-                <div className='container mx-auto flex lg:flex-row justify-center items-center mb-7 space-x-2'>
-                    <div className='container bg-fourthdary p-32 py-20 w-3/6 text-center'>Ads</div>
+                <div className='container mx-auto flex lg:flex-row justify-center items-center mb-7 '>
+                    <div className='container bg-fourthdary p-32 py-20 w-3/6 text-center mr-2'>Ads</div>
                     <div className='container bg-fourthdary p-32 py-20 w-3/6 text-center'>Events</div>
                 </div>
                 <div className='text-lg text-fourthdary font-bold pl-2 mb-2 border-l-8 border-fifthdary'>Featured Products</div>
@@ -71,16 +71,16 @@ export default function Welcome(props) {
                         <div className='bg-fourthdary rounded-br-md rounded-bl-md  container p-10 px-28'></div>
                     </div>
                 </div>
-                <div className='text-lg text-fourthdary font-bold pl-2 mb-2 border-l-8 border-fifthdary'>Shop by Categories</div>
-                <div className='grid grid-cols-6 grid-rows-2 auto-rows-max gap-3 mb-10'>
-                    <div className='text-center bg-fourthdary rounded-md p-20 col-start-1 col-end-3 row-start-1 row-end-3'>1</div>
-                    <div className='text-center bg-fourthdary rounded-md p-20 col-start-3 col-end-5'>2</div>
-                    <div className='text-center bg-fourthdary rounded-md p-20 col-start-5 col-end-7'>3</div>
-                    <div className='text-center bg-fourthdary rounded-md p-20 col-start-3 col-end-5'>4</div>
-                    <div className='text-center bg-fourthdary rounded-md p-20'>5</div>
-                    <div className='text-center bg-fourthdary rounded-md p-20'>6</div>
+                <div className='text-lg text-fourthdary font-bold pl-2 mb-3 border-l-8 border-fifthdary'>Shop by Categories</div>
+                <div className='grid grid-cols-6 grid-rows-2 auto-rows-max gap-2 mb-10'>
+                    <div className='text-center bg-fourthdary p-20 col-start-1 col-end-3 row-start-1 row-end-3'>1</div>
+                    <div className='text-center bg-fourthdary p-20 col-start-3 col-end-5'>2</div>
+                    <div className='text-center bg-fourthdary p-20 col-start-5 col-end-7'>3</div>
+                    <div className='text-center bg-fourthdary p-20 col-start-3 col-end-5'>4</div>
+                    <div className='text-center bg-fourthdary p-20'>5</div>
+                    <div className='text-center bg-fourthdary p-20'>6</div>
                 </div>
-                <div className='w-full bg-fourthdary rounded-md p-52'>
+                <div className='w-full bg-fourthdary p-52'>
 
                 </div>
             </main>
