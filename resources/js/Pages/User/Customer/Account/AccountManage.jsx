088399c3ca0ed1bc8c87f316a2aee3989e00f9ac
John-Dexter <johndexters1100@gmail.com>
2022-08-11 16:43:33 +0800

    Update AccountManage and AccountPassword Change

--- resources/js/Pages/User/Customer/Account/AccountManage.jsx
+++ resources/js/Pages/User/Customer/Account/AccountManage.jsx
@@ -1,6 +1,9 @@
 import AccountNav from '@/Layouts/AccountNav'
 import Authenticated from '@/Layouts/Authenticated'
 import React from 'react'
+import Input from '@/Components/Input'
+import logo from '@/images/logo.png'
+import Button from '@/Components/Button'
 
 export default function AccountManage(props) {
   return (
@@ -11,12 +14,73 @@ export default function AccountManage(props) {
       <AccountNav
         Username={props.auth.user.name}
       >
-        <div className='w-4/5 border-1 rounded shadow-xl m-5 divide-y'>
+        <div className='bg-primary w-4/5 border-1 rounded shadow-xl m-5 divide-y'>
           <div className='p-5 font-bold'>Manage Account</div>
-          <div className='p-5'>
-            Asd
-            
-            
+          <div className='p-5 flex flex-row'>
+            <form className='w-2/3 mt-10'>
+              <div className='grid grid-cols-2 gap-4 justify-end items-center' >
+                <div className='p-2 mt-1'><h2 className='font-semibold text-right'>Username:</h2></div>
+                <Input
+                  type="text"
+                  name="username"
+                  className="mt-1 w-full rounded-tr-xl border"
+                  autoComplete="name"
+                  isFocused={true}
+                />
+                <div className='p-2 mt-1'><h2 className='font-semibold text-right'>Name:</h2></div>
+                <Input
+                  type="text"
+                  name="name"
+                  value={props.auth.user.name}
+                  className="mt-1 w-full rounded-tr-xl border"
+                  autoComplete="name"
+                  isFocused={true}
+                />
+                <div className='p-2 mt-1'><h2 className='font-semibold text-right'>Email:</h2></div>
+                <Input
+                  type="email"
+                  name="email"
+                  className="mt-1 w-full rounded-tr-xl  border"
+                  autoComplete="name"
+                  isFocused={true}
+                />
+                <div className='p-2 mt-1'><h2 className='font-semibold text-right'>Phone Number:</h2></div>
+                <Input
+                  type="number"
+                  name="phonenumber"
+                  className="mt-1 w-full rounded-tr-xl border"
+                  autoComplete="name"
+                  isFocused={true}
+                />
+                <div className='p-2 mt-1'><h2 className='font-semibold text-right'>Gender:</h2></div>
+                <select
+                  name="gender"
+                  className=" w-full border px-3 py-2 rounded-tr-xl shadow-sm focus:outline-none"
+                >
+                  <option value=''>-Select Gender-</option>
+                  <option value='Male'>Male</option>
+                  <option value='Female'>Female</option>
+                </select>
+                <div className='p-2 mt-1'><h2 className='font-semibold text-right'>Date of Birth:</h2></div>
+                <Input
+                  type="text"
+                  name="birthday"
+                  className="mt-1 w-full rounded-tr-xl  border "
+                  autoComplete="name"
+                  isFocused={true}
+                />
+                <Button className='mt-5 col-start-2'>Confirm Changes</Button>
+              </div>
+              
+            </form>
+            <div className='flex flex-col w-1/3 text-center items-center'>
+              <div className=''>
+                <img className='w-32 h-32 mt-10 rounded-full' src='https://cdn-icons-png.flaticon.com/512/1946/1946429.png' />
+                <Button className='mt-2'>Upload Image</Button>
+                <h1>File Size Maximum: 2MB</h1>
+                <h1>File Extension: JPEG, PNG</h1>
+              </div>
+            </div>
           </div>
         </div>
       </AccountNav>
--- resources/js/Pages/User/Customer/Account/AccountPasswordChange.jsx
+++ resources/js/Pages/User/Customer/Account/AccountPasswordChange.jsx
@@ -1,20 +1,52 @@
 import React from 'react'
 import Authenticated from '@/Layouts/Authenticated'
 import AccountNav from '@/Layouts/AccountNav'
+import Input from '@/Components/Input'
+import Button from '@/Components/Button'
 export default function AccountPasswordChange(props) {
   return (
     <Authenticated
-    auth={props.auth}
-    errors={props.errors}
+      auth={props.auth}
+      errors={props.errors}
     >
-        <AccountNav
-          Username={props.auth.user.name}
-        >
-          <div className='w-4/5 border-1 rounded shadow-xl m-5 divide-y'>
-            <div className='p-5'>Change Password</div>
-            <div>Asd</div>
-          </div>     
-        </AccountNav>
+      <AccountNav
+        Username={props.auth.user.name}
+      >
+        <div className='w-4/5 border-1 rounded shadow-xl m-5 divide-y bg-primary'>
+          <div className='p-5'>Change Password</div>
+          <div className='p-5'>
+            <form>
+              <div className='flex flex-col'>
+                <div className='p-2 mt-1 rounded-bl-xl'><h2 className='font-semibold'>Current Password:</h2></div>
+                <Input
+                  type="password"
+                  name="currentpassword"
+                  className="mt-1 w-full rounded-tr-xl border"
+                  autoComplete="name"
+                  isFocused={true}
+                />
+                <div className='p-2 mt-1 rounded-bl-xl'><h2 className='font-semibold'>New Password:</h2></div>
+                <Input
+                  type="password"
+                  name="newpassword"
+                  className="mt-1 w-full rounded-tr-xl border"
+                  autoComplete="username"
+                />
+                <div className='p-2 mt-1 rounded-bl-xl'><h2 className='font-semibold'>Confirm Password:</h2></div>
+                <Input
+                  type="password"
+                  name="confirmpassword"
+                  className="mt-1 w-full rounded-tr-xl border"
+                  autoComplete="username"
+                />
+              </div>
+              <Button className='mt-5'>Confirm Changes</Button>
+            </form>
+
+
+          </div>
+        </div>
+      </AccountNav>
     </Authenticated>
   )
 }
