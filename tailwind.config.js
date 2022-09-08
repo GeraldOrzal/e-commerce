9fb207f426a3cc74a76c4c6d191483f0e3d74ec0
GeraldOrzal <geraldorzal18@gmail.com>
2022-09-08 15:16:20 +0800

    clean

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,12 +27,12 @@ module.exports = {
         },
         extend: {
             backgroundImage:{
-                category0:'url("./public/images/clothes.jpg")',
-                category1:'url("./public/images/furnitures.jpeg")',
-                category2:'url("./public/images/foods.jpg")',
-                category3:'url("./public/images/technology.webp")',
-                category4:'url("./public/images/hygiene.jpeg")',
-                category5:'url("./public/images/medicines.webp")',
+                category0:'url("https://sadsadasdasdasddsfdsf.herokuapp.com/public/images/clothes.jpg")',
+                category1:'url("https://sadsadasdasdasddsfdsf.herokuapp.com/public/images/furnitures.jpeg")',
+                category2:'url("https://sadsadasdasdasddsfdsf.herokuapp.com/public/images/foods.jpg")',
+                category3:'url("https://sadsadasdasdasddsfdsf.herokuapp.com/public/images/technology.webp")',
+                category4:'url("https://sadsadasdasdasddsfdsf.herokuapp.com/public/images/hygiene.jpeg")',
+                category5:'url("https://sadsadasdasdasddsfdsf.herokuapp.com/public/images/medicines.webp")',
                 sample:'url("https://images.unsplash.com/photo-1554629947-334ff61d85dc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1024&h=1280&q=80")',
                 ads:'url("./public/images/ads.png")',
                 f21:'url("./public/images/forever-21.png")',
