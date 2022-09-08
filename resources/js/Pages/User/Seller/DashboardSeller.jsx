06fc1322db2cc9068f2bb63aea3f13bd7ace94fe
John-Dexter <johndexters1100@gmail.com>
2022-09-08 10:44:01 +0800

    09/08

--- resources/js/Pages/User/Seller/DashboardSeller.jsx
+++ resources/js/Pages/User/Seller/DashboardSeller.jsx
@@ -65,7 +65,7 @@ export default function Dashboard(props) {
                         </div>
 
 
-            >
+            </div>
                 <div className='container-fluid flex-1 flex-col p-5 space-y-5'>
                     Welcome Back!
                     <div className='flex flex-row items-center space-x-3'>
@@ -120,6 +120,7 @@ export default function Dashboard(props) {
 
                     </div>
                 </div>
+            </div>
             </div>
 
         </Authenticated>
